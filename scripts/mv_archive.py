093f0e4d2ce3c8f684d764e8f7c7385a523b74e6
An-Death <saenarayoursorrow@gmail.com>
2017-10-03 11:36:03 +0300

    reliase v 1.0 =))

--- scripts/mv_archive.py
+++ scripts/mv_archive.py
@@ -11,12 +11,13 @@ import datetime
 
 def docs(script_name, help=False):
     if help:
-        print('{n}{script_name} {n}'.format(script_name=script_name, n='\n') )
+        print('{n}{script_name} {n}'.format(script_name=script_name, n='\n'))
         print(main.__doc__)
         print('USAGE: puthon {} {}'.format(script_name, ' '.join(['{some_dir}', '{dest_dir}'])))
     else:
         print('USAGE: puthon {} {}'.format(script_name, ' '.join(['{some_dir}', '{dest_dir}'])))
     exit(0)
+
 
 def check_input_args(*args):
     """
@@ -36,26 +37,24 @@ def check_input_args(*args):
 
 
 def check_dest_path(dest):
-
     if dest.islink():
         dest = dest.readlinkabs()
-
 
     if not dest.isdir():
         exit('Указанный пусть назначения не существует! Проверьте путь {}!'.format(dest))
     elif not dest.parent.ismount():
         exit('Проверьте mount hdd /media/hdd!')
 
-def get_datetime(file_name):
 
-    dt = datetime.datetime.strftime(file_name[:16], '%Y%m%d_%H%M%S_')
-    return dt.date(), dt.hour
+def get_datetime(file_name):
+    dt = datetime.datetime.strptime(file_name[:16], '%Y%m%d_%H%M%S_')
+    return dt.date(), dt.time()
 
 
 def main():
     """
-Скрипт для переноса восстановленного архива из обего хранилища по попкам.
-ринимает:
+Скрипт для переноса восстановленного архива из общего хранилища по папкам.
+Принимает:
 {Первой переменной} : Путь до восстановленного архива
 {Второй переменной} : Путь куда перенести архив создав новое древо подобно коннекту {date}/{hour}/{file}
 """
@@ -63,15 +62,22 @@ def main():
     store, dest = check_input_args(*sys.argv)
     check_dest_path(dest)
     for file in store.walk('*.flv'):
-        date, hour = get_datetime(file)
+        path_to_new_store = dest
+        date, time = get_datetime(file.name)
         date = date.isoformat()
-        if not (dest + date).exists():
-            (dest + date).mkdir()
-        if not (dest + date + hour).exists():
-            (dest + date + hour).mkdir()
-        file.copy(dest + date + hour)
+        hour = time.isoformat()[:2]
+        path_to_new_store = path_to_new_store + date + '/'
+        if not path_to_new_store.exists():
+            path_to_new_store.mkdir()
+        path_to_new_store = path_to_new_store + hour
+        if not path_to_new_store.exists():
+            path_to_new_store.mkdir()
+        # file.copy(path_to_new_store)
+        try:
+            file.move(path_to_new_store)
+        except Exception:
+            continue
 
 
 if __name__ == '__main__':
     main()
-
