4c55538083551ca9c862385a51a3bf1c0e1c86e9
An-Death <saenarayoursorrow@gmail.com>
2017-10-25 16:19:27 +0300

    Заполнил основной алгоритм распределения эвентов из таблички лога

--- user_activity_report/user_activity_report.py
+++ user_activity_report/user_activity_report.py
@@ -8,7 +8,10 @@ from base_models.wits_models import (Wits_user_log as log,
                                      Wits_user_event as event)
 from projects import project
 
-PERIOD = {'from': '2017-08-31', 'to': '2017-10-01'}
+# todo Сделать вводом из формы или cli
+FROM = '2017-08-31'
+TO = '2017-10-01'
+######################################################################
 
 bke = project.Project('bke')
 bke.configurate()
@@ -20,7 +23,7 @@ def close_all_active_session(date: Dt, users_dict):
     pass
 
 
-limit = {'start': Dt(PERIOD['from']), 'stop': Dt(PERIOD['to'])}
+limit = {'start': Dt(FROM), 'stop': Dt(TO)}
 log_table = session.query(log.user_id, log.date, log.data, log.event_id)
 log_table = log_table.filter(log.date.between(limit['start'].to_request(), limit['stop'].to_request()))
 log_table = log_table.filter(log.data.notlike('%=wchange=%')).filter(log.data.notlike('%=vload=%'))
@@ -28,17 +31,33 @@ log_table = log_table.filter(log.data.notlike('%=wchange=%')).filter(log.data.no
 
 event_table = session.query(event.id, event.name_en).all()
 event_dict = {k: v for v, k in event_table}
+u = USERS
 
 for user_id, date, data, event_id in log_table.all():
 
-    if user_id != -1 and user_id not in USERS:
+    if user_id not in USERS and user_id > 0:
         USERS[user_id] = User(user_id, session)
+
+    user = USERS[user_id]
 
     if event_id == event_dict['Stop'] and user_id == -1:
         close_all_active_session(Dt(date), USERS)
-    elif event_id in (event_dict[i] for i in ['Logout', 'Session timeout', 'Stop']):
-        pass
+    elif event_id in (event_dict[i] for i in ['Logout', 'Session timeout']):
+        if not user.is_logged(): continue
+        # todo Написать интерфейс для сессии
+        user.session[data].close(date)
+        user.session_stop()
     elif event_id == event_dict['Session killed']:
-        pass
+        if not user.is_logged(): continue
+        active_session, new_session = data.split('=')
+        user.session[active_session].close(date)
+        user.session_stop()
     elif event_id == event_dict['Login']:
+        if user.is_logged():
+            user.close_active_session()
+            user.session_stop()
+
+        user.session[data].start(date)
+        user.session_start()
+    elif event_id == event_dict['User action']:
         pass
