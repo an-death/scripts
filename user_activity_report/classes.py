84e97ec41d6757d3b4206e3fb145a5f4bf7470bb
An-Death <saenarayoursorrow@gmail.com>
2017-10-26 14:12:53 +0300

    Добавил eq и str для класса Session Фикс багов: 1. Получени сесси из get_active_session 2. Фикс бага по подсчёту тотал тайма в видео 3. Фиксы многочисленных итераций dict// Забывал .values()

--- user_activity_report/classes.py
+++ user_activity_report/classes.py
@@ -11,10 +11,11 @@ class User:
         self.last_name = self.param.last_name
         self.first_name = self.param.first_name
         self.patr_name = self.param.patr_name
-        self._sessions = defaultdict(Session)
+        self._sessions = {}
         self.logged = False
         self.total_video_time = 0
         self.total_monitoring_time = 0
+        self.collisions = []
         # self.id = id
         # self.network_id = user.network_id
         # self.name = user.name
@@ -36,7 +37,8 @@ class User:
     def info(self):
         return '{id}\n{org}\n{pos} \n{name}'.format(id=self.id,
                                                     pos=self.param.position,
-                                                    name=' '.join([self.last_name, self.first_name, self.patr_name]),
+                                                    name=' '.join(str(i) for i in
+                                                                  [self.last_name, self.first_name, self.patr_name]),
                                                     org=self.param.organization)
 
     def is_logged(self):
@@ -49,35 +51,37 @@ class User:
         self.logged = False
 
     def sessions(self, ses):
-        return self._sessions[ses]
+        try:
+            session = self._sessions[ses]
+        except KeyError:
+            session = self._sessions[ses] = Session(ses)
+        return session
 
     def session_start(self, session, dt):
-        try:
-            assert not self.sessions(session).status(), 'Пытаемся открыть уже открутую сессию! {}'.format(session)
-        except KeyError:
-            # Если сесси нет, мы её создаём
-            self.sessions(session)
+        assert not self.sessions(session).status(), 'Пытаемся открыть уже открутую сессию! {}'.format(session)
         self.sessions(session).open(dt)
         self.login()
 
-    def session_stop(self, session, dt):
+    def session_stop(self, session: str, dt):
         active_session = self.get_active_session()
-        assert active_session is self.sessions(session), 'Сессия на закрытие не соответствует активной сессии\n' \
-                                                         'user: {}' \
-                                                         'active: {}\n' \
+        new_session = self.sessions(session)
+        assert active_session == new_session, 'Сессия на закрытие не соответствует активной сессии\n' \
+                                              'user: {}\n' \
+                                              'active: {}\n' \
                                                          'session: {}\n' \
                                                          ''.format(self.info(), active_session, self.sessions(session))
         assert active_session.status(), 'Пытаемся закрыть сессию. Сессия уже закрыта! {}'.format(session)
         # todo ^ Описать решние коллизий в таком случае ^
-        active_session.stop(dt)
+        active_session.close(dt)
         self.logout()
 
     def session_store(self, session: str, args: str):
-        assert self.sessions(session).status(), 'Текущая сессия закрыта.\n{}\nНе можем добавить запись в сессию!'
+        assert self.sessions(
+            session).status(), 'Текущая сессия закрыта.\n{}\nНе можем добавить запись в сессию!'.format(session)
         self.sessions(session).store(args)
 
     def get_active_session(self):
-        session = [session for session in self._sessions if session.status()]
+        session = [session for session in self._sessions.values() if session.status()]
         assert len(session) == 1, 'Активной дожна быть только одна сессия! Найдено актиных сессий: ' \
                                   '\n{} ' \
                                   '\nДля пользователя: ' \
@@ -89,10 +93,10 @@ class User:
         active_session = self.get_active_session()
         if not dt:
             dt = active_session.get_cached_date()
-        self.session_stop(active_session, dt)
+        self.session_stop(active_session.ses, dt)
 
     def calculate_total(self):
-        for session in self._sessions:
+        for session in self._sessions.values():
             self.total_video_time += session.total_time_video
             self.total_monitoring_time += session.total_time
         return self.total_monitoring_time, self.total_video_time
@@ -104,24 +108,36 @@ class Session:
         self.total_time = 0
         self.total_time_video = 0
         self.planshet = defaultdict(int)
-        self.open = False
+        self.active = False
         self.cached_data = {'start_session': 0, 'last': 0}
         self.storage = {'all': {'total': 0}, 'video': {'start': 0, 'stop': 0}}
+
+    def __str__(self):
+        return '{}.{}'.format(self.ses, self.active)
+
+    def __eq__(self, other):
+        if not isinstance(other, Session):
+            return False
+        else:
+            if self.ses == other.ses:
+                return True
+            else:
+                return False
 
     def close(self, dt):
         self.cached_data['last'] = dt
         self.total_time = self.cached_data['last'] - self.cached_data['start_session']
-        self.open = False
+        self.active = False
 
     def open(self, dt):
         self.cached_data['start_session'] = dt
-        self.open = True
+        self.active = True
 
     def store(self, args: str):
         def calculate(start, stop):
             total = stop - start
             self.total_time_video += total
-            stop, start = 0, 0
+            self.storage['video']['stop'], self.storage['video']['start'] = 0, 0
 
         time, action, form = args.split('=')
         dt = Dt(time)
@@ -129,12 +145,12 @@ class Session:
             start = self.storage['video']['start']
             stop = self.storage['video']['stop']
             if action == 'fopen' and not start:
-                start = dt
+                self.storage['video']['start'] = dt
             elif action == 'fclose' and start:
                 calculate(start, dt)
             elif action == 'fopen' and not stop:
                 calculate(start, dt)
-                start = dt
+                self.storage['video']['start'] = dt
                 # Остальные формы игнорим.
                 # todo сделать подсчёт по всем формам
 
@@ -143,7 +159,7 @@ class Session:
         Возвращает True/False для открытой и закрытой сесси
         :return: Bool 
         """
-        return self.open
+        return self.active
 
     def get_cached_date(self):
         return self.cached_data['last'] or self.cached_data['start_session']
