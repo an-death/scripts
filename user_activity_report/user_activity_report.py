620e9323a1cca41e231ee683e7a892f5bb65539e
An-Death <saenarayoursorrow@gmail.com>
2017-10-26 14:15:20 +0300

    Добавил ограничение по эвентам в sql выборке из таблицы. Исключил не влиящие эвенты. Добавил лист с коллизиями и игнор коллизий в таблице (когда сессия закорывается раньше, чем открывается)

--- user_activity_report/user_activity_report.py
+++ user_activity_report/user_activity_report.py
@@ -30,6 +30,7 @@ def get_events_table(session):
 def get_wits_user_log_data(session, limit):
     log_table = session.query(log.user_id, log.date, log.data, log.event_id)
     log_table = log_table.filter(log.date.between(limit['start'].to_request(), limit['stop'].to_request()))
+    log_table = log_table.filter(log.event_id.notin_([5, 6, 9]))
     log_table = log_table.filter(log.data.notlike('%=wchange=%')).filter(log.data.notlike('%=vload=%'))
     # print('\n'.join([str(row) for row in log_table.all()]))
     return log_table
@@ -54,14 +55,18 @@ def main(u: USERS, p: PROJECT):
 
         if user_id not in u and user_id > 0:
             u[user_id] = User(user_id, dbconnection)
-
-        user = u[user_id]
-
         # В случае рестарта стрима - обрываем все активные сессии всех пользователей
         if event_id == event_dict['Stop'] and user_id == -1:
             close_all_active_session(date, u)
-        elif event_id in (event_dict[i] for i in ['Logout', 'Session timeout']):
-            if not user.is_logged(): continue
+            continue
+
+        user = u[user_id]
+        if event_id in (event_dict[i] for i in ['Logout', 'Session timeout']):
+            if not user.is_logged():
+                # Сохраняем коллизии
+                # Иногда сессия может закрыться ранее, чем открыться.
+                user.collisions.append(data.replace('!', ' ').replace('=', ' ').split()[0])
+                continue
             # todo Написать интерфейс для сессии
             # user.session[data].close(date)
             user.session_stop(data, date)
@@ -71,14 +76,23 @@ def main(u: USERS, p: PROJECT):
             # user.session[active_session].close(date)
             user.session_stop(active_session, date)
         elif event_id == event_dict['Login']:
+            if data.replace('!', ' ').replace('=', ' ').split()[0] in user.collisions:
+                # Игнорируем сессии, которые закрылись ранее, чем открылись
+                continue
             if user.is_logged():
                 user.close_active_session()
                 # user.session_stop()
             # user.session[data].start(date)
             user.session_start(data, date)
         elif event_id == event_dict['User action']:
+            if not user.is_logged(): continue
             active_session, args = data.split('!')
             user.session_store(active_session, args)
+
+    close_all_active_session(date, u)  # Закрываем все не закрытые сессии последней полученной датой.
+    for user in u.values():
+        total, total_video = user.calculate_total()
+        print(user.info(), 'total: {}\n total_video: {}'.format(total, total_video))
 
 
 if __name__ == '__main__':
