9faffcddb8936c39953b4a74af11d22b43edf098
An-Death <saenarayoursorrow@gmail.com>
2017-10-27 16:24:41 +0300

    Реализовал запись в фаил. Скрипт можно считать рабочим. v 0.1

--- user_activity_report/user_activity_report.py
+++ user_activity_report/user_activity_report.py
@@ -2,7 +2,9 @@
 
 from collections import defaultdict
 
+import pandas as pd
 from classes import User, Dt, get_connect_to_db
+from users_report import get_table, aliases
 
 from base_models.wits_models import (Wits_user_log as log,
                                      Wits_user_event as event)
@@ -33,6 +35,65 @@ def close_all_active_session(date: Dt, users_dict):
     for user in users_dict.values():
         if user.is_logged():
             user.close_active_session(date)
+
+
+def create_users_activity_dict(users: dict):
+    # Подсчитываем время
+    users_activity_dict = {}
+    for u in users.values():
+        u.calculate_total()
+        k = u.fio() or u.param.name
+        users_activity_dict[k] = {'video': u.total_video_time.to_human(),
+                                  'total': u.total_monitoring_time.to_human()}
+    return users_activity_dict
+
+
+def write_sheet(sheet_name, writer, table):
+    table.to_excel(writer, sheet_name=sheet_name, index_label='№п/п', startrow=1, header=False)
+    book = writer.book
+    # Add a header format.
+    header_format = book.add_format({
+        'bold': True,
+        'text_wrap': True,
+        'align': 'center',
+        'valign': 'top',
+        'fg_color': '#D7E4BC',
+        'border': 2})
+    formats = {
+        'fio': book.add_format({'text_wrap': True, 'border': 1}),
+        'data': book.add_format({
+            'text_wrap': True,
+            'align': 'center',
+            'valign': 'center',
+            'border': 1})
+    }
+
+    sheet = writer.sheets[sheet_name]
+    # Записываем шапку
+    col = ['№п/п', 'ФИО', 'Видео', 'Всего'] if sheet_name != 'Пользователи' else aliases.values()
+    for col_num, value in enumerate(col):
+        sheet.write(0, col_num, value, header_format)
+    # Форматируем данные
+    if sheet_name != 'Пользователи':
+        sheet.set_column('B:B', 30, formats['fio'])
+        sheet.set_column('C:C', 20, formats['data'])
+        sheet.set_column('D:D', 20, formats['data'])
+    else:
+        sheet.set_column('B:B', 30, formats['fio'])
+        sheet.set_column('C:C', 18, formats['fio'])
+        sheet.set_column('D:D', 38, formats['fio'])
+        sheet.set_column('E:E', 38, formats['fio'])
+        sheet.set_column('F:F', 20, formats['fio'])
+        sheet.set_column('G:G', 15, formats['fio'])
+        sheet.set_column('H:H', 28, formats['fio'])
+
+
+def create_xlsx(file_name, user_table, activity_table):
+    with pd.ExcelWriter(file_name, engine='xlsxwriter') as writer:
+        write_sheet('Пользователи', writer, user_table)
+        sheet_name = 'Сентябрь'  # todo сделать автоматическое опеределение месяца
+        write_sheet(sheet_name, writer, activity_table)
+
 
 
 def main(u: USERS, p: PROJECT):
@@ -81,11 +142,15 @@ def main(u: USERS, p: PROJECT):
             if not user.is_logged(): continue
             active_session, args = data.split('!')
             user.session_store(active_session, args)
+    # Закрываем все не закрытые сессии последней полученной датой.
+    close_all_active_session(date, u)
+    table = create_users_activity_dict(u)
+    table = pd.DataFrame(table)
+    table = table.unstack().unstack().reset_index()
+    table = table.reindex(columns=['index', 'video', 'total'])
+    user_table = get_table(dbconnection)
+    create_xlsx('Список пользователей GTI-online.xlsx', user_table, table)
 
-    close_all_active_session(date, u)  # Закрываем все не закрытые сессии последней полученной датой.
-    for user in u.values():
-        total, total_video = user.calculate_total()
-        print(user.info(), 'total: {}\n total_video: {}'.format(total, total_video))
 
 
 if __name__ == '__main__':
