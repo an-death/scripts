154aac8ac7d392daf22ff48e4d6c5ff1f191930a
An-Death <saenarayoursorrow@gmail.com>
2017-10-30 11:53:13 +0300

    Переписал цикл распределения записи из базы. fix. Отловил редкий баг не закрытия сессии Дописал чистку файла отчёта для удаления border ниже таблиц. Дописал комментов.

--- user_activity_report/user_activity_report.py
+++ user_activity_report/user_activity_report.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 import pandas as pd
 from classes import User, Dt, get_connect_to_db
 from users_report import get_table, aliases
+from xlsxwriter.utility import xl_range
 
 from base_models.wits_models import (Wits_user_log as log,
                                      Wits_user_event as event)
@@ -19,34 +20,36 @@ USERS = defaultdict(User)
 
 
 def get_events_table(session):
+    """
+    Дёргаем из базы таблицу WITS_USER_EVENT и приобразуем её в словарь
+    :param session: 
+    :return: Dict('Stop': 7)
+    """
     event_table = session.query(event.id, event.name_en).all()
-    return {k: v for v, k in event_table}
+    return {name: _id for _id, name in event_table}
 
 
 def get_wits_user_log_data(session, limit):
     log_table = session.query(log.user_id, log.date, log.data, log.event_id)
     log_table = log_table.filter(log.date.between(limit['start'].to_request(), limit['stop'].to_request()))
     log_table = log_table.filter(log.event_id.notin_([5, 6, 9]))
-    # log_table = log_table.filter(log.data.notlike('%=wchange=%')).filter(log.data.notlike('%=vload=%'))
     # Для отловли коллизий
-    # log_table = log_table.filter(log.user_id.in_([-1,214]))
-    # todo Передавать в пандас не таблицу, а запрос?? v 0.2
-    # query_as_string = str(log_table.statement.compile(compile_kwargs={"literal_binds": True}))
+    # log_table = log_table.filter(log.user_id.in_([-1,63]))
     return log_table.all()
 
 
 def close_all_active_session(date: Dt, users_dict):
     for user in users_dict.values():
-        if user.is_logged():
+        if user.is_logged:
             user.close_active_session(date)
 
 
-def create_users_activity_dict(users: dict):
+def users_activity_as_dict(users: dict):
     # Подсчитываем время
     users_activity_dict = {}
     for u in users.values():
         u.calculate_total()
-        k = u.fio() or u.param.name
+        k = u.fio or u.param.name
         users_activity_dict[k] = {'video': u.total_video_time.to_human(),
                                   'total': u.total_monitoring_time.to_human()}
     return users_activity_dict
@@ -64,6 +67,7 @@ def write_sheet(sheet_name, writer, table):
         'fg_color': '#D7E4BC',
         'border': 2})
     formats = {
+        'clean': book.add_format({'border': None}),
         'fio': book.add_format({'text_wrap': True, 'border': 1}),
         'data': book.add_format({
             'text_wrap': True,
@@ -73,6 +77,8 @@ def write_sheet(sheet_name, writer, table):
     }
 
     sheet = writer.sheets[sheet_name]
+    first_row = 1 + len(table.index)
+    clean_range = xl_range(first_row, 0, first_row * 10, len(table.columns))
     # Записываем шапку
     col = ['№п/п', 'ФИО', 'Видео', 'Всего'] if sheet_name != 'Пользователи' else aliases.values()
     for col_num, value in enumerate(col):
@@ -90,25 +96,37 @@ def write_sheet(sheet_name, writer, table):
         sheet.set_column('F:F', 20, formats['fio'])
         sheet.set_column('G:G', 15, formats['fio'])
         sheet.set_column('H:H', 28, formats['fio'])
+    # clean all below data
+    sheet.conditional_format('B1', {'type': 'blanks',
+                                    'multi_range': clean_range,
+                                    'format': formats['clean']})
 
 
 def create_xlsx(file_name, user_table, activity_table):
     with pd.ExcelWriter(file_name, engine='xlsxwriter') as writer:
         write_sheet('Пользователи', writer, user_table)
-        # sheet_name = 'Сентябрь'  # todo сделать автоматическое опеределение месяца
+        # sheet_name = 'Сентябрь'  # todo сделать автоматическое опеределение месяца и цикл
         write_sheet(SHEET_NAMES[-1], writer, activity_table)
 
 
 def main(u: USERS, p: PROJECT):
+    # todo Дробить промежутки по месяцам
     limit = {'start': Dt(FROM), 'stop': Dt(TO)}
 
+    # Создаём сессию подключения к базе, передавая шорткат проекта
     dbconnection = get_connect_to_db(p)
+    # Дёргаем из базы таблицу WITS_USER_EVENT и приобразуем её в словарь
     event_dict = get_events_table(dbconnection)
+    # Возвращаем список всез записей из WITS_USER_LOG в пределах limits
     log_table = get_wits_user_log_data(dbconnection, limit)
 
+    # ===============================================================================================================
+    # START CYCLE
     for user_id, date, data, event_id in log_table:
         date = Dt(date)
 
+        # Если юзера нету в словаре и это не сервер (u_id = -1)
+        # Создаём юзера и добавляем в словарь
         if user_id not in u and user_id > 0:
             u[user_id] = User(user_id, dbconnection)
         # В случае рестарта стрима - обрываем все активные сессии всех пользователей
@@ -117,42 +135,41 @@ def main(u: USERS, p: PROJECT):
             continue
 
         user = u[user_id]
-        if event_id in (event_dict[i] for i in ['Logout', 'Session timeout']):
-            if not user.is_logged():
-                # Сохраняем коллизии
-                # Иногда сессия может закрыться ранее, чем открыться.
+        stop_cond = (event_dict[i] for i in ['Logout', 'Session timeout'])
+
+        if user.is_logged:
+            if event_id in stop_cond:
+                user.session_stop(data, date)
+            elif event_id == event_dict['Session killed']:
+                active_session, new_session = data.split('=')
+                user.session_stop(active_session, date)
+            elif event_id == event_dict['Login']:
+                # Stop active session because user already is logged!
+                user.close_active_session(date)
+                # Start new session
+                user.session_start(data, date)
+            elif event_id == event_dict['User action']:
+                active_session, args = data.split('!')
+                user.session_store(active_session, date, args)
+        else:  # user.is_logout!
+            if event_id in stop_cond:
+                # user tried logout before login
+                # save collision
+                # todo make collision as interface to hide realisation!
                 user.collisions.append(data.replace('!', ' ').replace('=', ' ').split()[0])
                 continue
-            # todo Написать интерфейс для сессии
-            # user.session[data].close(date)
-            user.session_stop(data, date)
-        elif event_id == event_dict['Session killed']:
-            if not user.is_logged(): continue
-            active_session, new_session = data.split('=')
-            # user.session[active_session].close(date)
-            user.session_stop(active_session, date)
-        elif event_id == event_dict['Login']:
-            if data.replace('!', ' ').replace('=', ' ').split()[0] in user.collisions:
-                # Игнорируем сессии, которые закрылись ранее, чем открылись
-                continue
-            if user.is_logged():
-                user.close_active_session(date)
-                # user.session_stop()
-            # user.session[data].start(date)
-            user.session_start(data, date)
-        elif event_id == event_dict['User action']:
-            if not user.is_logged(): continue
-            active_session, args = data.split('!')
-            user.session_store(active_session, date, args)
+            if event_id == event_dict['Login'] and data not in user.collisions:
+                user.session_start(data, date)
+    # ===================================================================================================================
+    # CYCLE END
+
     # Закрываем все не закрытые сессии последней полученной датой.
-    close_all_active_session(date, u)
-    table = create_users_activity_dict(u)
-    table = pd.DataFrame(table)
+    # close_all_active_session(date, u)
+    table = pd.DataFrame(users_activity_as_dict(u))
     table = table.unstack().unstack().reset_index()
     table = table.reindex(columns=['index', 'video', 'total'])
     user_table = get_table(dbconnection)
     create_xlsx('reports/Список пользователей GTI-online.xlsx', user_table, table)
-
 
 
 if __name__ == '__main__':
